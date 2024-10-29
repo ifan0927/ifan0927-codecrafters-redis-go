d611cd524a6627fcc455a0d486e9243270924969
ifan <ifan@estate.tw>
2024-10-29 21:58:55 +0800

    multiple ping

--- app/server.go
+++ app/server.go
@@ -19,14 +19,19 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	c, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		handleClient(c)
 	}
+}
 
-	c.Write([]byte("+PONG\r\n"))
-	c.Write([]byte("+PONG\r\n"))
+func handleClient(conn net.Conn) {
+	defer conn.Close()
 
+	conn.Write([]byte("+PING\r\n"))
 }
