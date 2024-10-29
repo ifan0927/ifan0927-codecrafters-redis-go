dab95fd432f855589a8b3bd4ec761450eb4b2ccc
ifan <ifan@estate.tw>
2024-10-29 21:11:25 +0800

    uncomment

--- app/server.go
+++ app/server.go
@@ -11,7 +11,7 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	// You can use print statements as follows for debugging, they'll be visible when running tests. 123
 	fmt.Println("Logs from your program will appear here!")
 
 	Uncomment this block to pass the first stage
@@ -21,7 +21,7 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	_, err = l.Accept() 
 	if err != nil {
 	 	fmt.Println("Error accepting connection: ", err.Error())
 	 	os.Exit(1)
