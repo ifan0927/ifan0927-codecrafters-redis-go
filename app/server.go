41ce89ad2db508a700e17cc5d1b98cce0f48fd58
ifan <ifan@estate.tw>
2024-10-29 23:09:30 +0800

    multiple ping

--- app/server.go
+++ app/server.go
@@ -19,19 +19,19 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-
-	c, err := l.Accept()
 	for {
+		c, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go handlerequest(c)
+		handlerequest(c)
 	}
 
 }
 
 func handlerequest(conn net.Conn) {
+	defer conn.Close()
 	buffer := make([]byte, 1024)
 	for {
 		// 讀取請求
